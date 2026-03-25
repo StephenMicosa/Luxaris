9c90a25714ce44f3ab5318429082169e0187ee87
Stephen Micosa <stephenmicosa02@gmail.com>
2026-03-25 16:48:57 +0100

    refactor : suppression du composant MannequinGrid et des fichiers de données associés

--- src/data/mannequinsData.js
+++ src/data/mannequinsData.js
@@ -1,14 +1,1 @@
-export const mannequinTypes = [
-  {
-    id: 1,
-    title: "Mannequin réel",
-    description:
-      "Des mannequins professionnels pour vos shootings, campagnes, défilés et contenus de marque.",
-  },
-  {
-    id: 2,
-    title: "Mannequin IA",
-    description:
-      "Des mannequins virtuels créés avec l’intelligence artificielle pour produire rapidement des visuels premium et innovants.",
-  },
-];
+export { mannequinsData } from "./mannequinsData.jsx";
