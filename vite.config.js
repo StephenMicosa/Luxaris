789a5ab7f0f890888e48390d84b75bf412de21ea
Stephen Micosa <stephenmicosa02@gmail.com>
2026-04-15 22:18:15 +0200

    fix: ajout du fichier _redirects pour rediriger vers index.html ; mise à jour du chemin de base dans vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Luxaris/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
